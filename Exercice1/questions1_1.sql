9fbbc448ca70e4b7d88202288441a1d870732904
raphael.thieffry <raphael.thieffry@gmail.com>
2024-12-17 14:35:17 +0100

    ex1

--- Exercice1/questions1_1.sql
+++ Exercice1/questions1_1.sql
@@ -1,15 +1,17 @@
 -- 3. Exprimez les requêtes suivantes dans la fenêtre d’exécution et exécutez les.
 
 -- (a) Le nom et prénom du client numéro 5
-
+SELECT cli_nom, cli_prenom FROM client WHERE cli_id = 5;
 
 -- (b) Les jours o`u le client numéro 5 a occupé une chambre
-
+SELECT jour FROM occupation WHERE cli_id = 5;
 
 -- (c) Les chambres occupées le 1999-01-22
-
+SELECT chb_id FROM occupation WHERE jour = '1999-01-22';
 
 -- (d) Le nom et prénom des clients ayant pris une chambre le 1999-01-22
+SELECT cli_nom, cli_prenom FROM occupation o, client cl WHERE 
+    cl.cli_id = o.cli_id AND jour = '1999-01-22';
 
 --4. Pour chacune des requêtes précédentes :
 -- • Obtenez le plan d’exécution retourné par le SGBD

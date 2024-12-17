9148e6c33f04c57b3082da27b0d522ef35598e10
anto <antoninrussac1@gmail.com>
2024-12-17 14:13:10 +0100

    q1

---
+++ Exercice1/questions.sql
@@ -0,0 +1,12 @@
+-- 3. Exprimez les requêtes suivantes dans la fenêtre d’ex ́ecution et exécutez les.
+
+-- (a) Le nom et pr ́enom du client num ́ero 5
+
+
+-- (b) Les jours o`u le client num ́ero 5 a occup ́e une chambre
+
+
+-- (c) Les chambres occup ́ees le 1999-01-22
+
+
+-- (d) Le nom et pr ́enom des clients ayant pris une chambre le 1999-01-22
